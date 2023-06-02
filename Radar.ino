1440c5cd52a8930134658a40cb54a8c3ee7c01d7
Amr Mahmoud <dramr2852001@gmail.com>
2023-06-02 14:31:03 +0300

    Changed Ultrasonic pins

--- Radar.ino
+++ Radar.ino
@@ -1,8 +1,8 @@
 // Includes the Servo library
 #include <Servo.h>.
 // Defines Tirg and Echo pins of the Ultrasonic Sensor
-const int trigPin = 10;
-const int echoPin = 11;
+const int trigPin = 3;
+const int echoPin = 5;
 // Variables for the duration and the distance
 long duration;
 int distance;
